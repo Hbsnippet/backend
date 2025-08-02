650d318d9fb814c3c3d9b06b9371172436ab85bb
Lukeburner_ <hamdanbashar07@gmail.com>
2025-08-03 00:16:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('lukeburner')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login at lukeburners dengen</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>chai aur code</h2>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
